95e1917f71b908efb06517d868f8bfbb1417e2aa
Mohammed SALEM <salem.mhmed@yahoo.com>
2022-10-11 19:24:00 +0000

    first commit

---
+++ build_images.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f1823ba3c110addb6deb4c42ad028f94afc983284c105d5c0150926da11b7a6
+size 158
---
+++ run_containers.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc82ebe05b7ccabdcab2dc27280e62170b18fceb5995e277205eb4cfc43976ca
+size 306
