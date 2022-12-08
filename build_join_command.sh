7218393192a7754d26569df5f9b1263cbe890d3d
root <root@controle-plane.k8s.local>
2022-12-08 18:26:49 -0500

    join command

--- build_join_command.sh
+++ build_join_command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read 'master ip address : ' ipaddr
-sha_token = "$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')"
-token = "$(kubeadm token list | awk '{print $1}' | sed -n '2 p')"
+read -p 'master ip address : ' ipaddr
+sha_token="$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')"
+token="$(kubeadm token list | awk '{print $1}' | sed -n '2 p')"
 echo "kubeadm join $ipaddr:6443 --token=$token --discovery-token-ca-cert-hash sha256:$sha_token"
